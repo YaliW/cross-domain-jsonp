cc95450d58cbd9c7a408f070fb67ed586c9e5ee1
YaliW <wangyalijob@163.com>
2020-02-26 21:42:23 +0800

    add jsonp

---
+++ README.md
@@ -0,0 +1,15 @@
+# cross-domain-jsonp
+使用原生js 创建script标签实现 jsonp跨域   
+服务器使用express创建
+
+## 安装
+```
+npm init
+npm install --save-dev express
+
+```
+
+## 运行
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+// back-end
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+app.use('/jsonp', (req, res, next) => {
+    const method = req.query.method;
+    const callbackName = req.query.callback;
+    const args = JSON.parse(req.query.args);
+    let result = 0;
+    if (method === 'sum') {
+        const sum = exports[method];
+        result = sum.call(this, args);
+        res.type('.js');
+        res.send(callbackName + '(' + JSON.stringify(result) + ')');
+    } else {
+        res.status(400);
+        res.send('args not match the right formate');
+    }
+});
+
+app.listen(8080, () => {
+    console.log('The server is running at http://localhost:8080"');
+});
+
+exports.sum = function(args) {
+    return args.a + args.b;
+}
